cd5ec5e3f1865dac98ce00163e6b02a5a48126a5
Okechukwu <okei.muokwugwo@outlook.com>
2022-09-27 19:54:27 +0100

    Import more modules in the entry point

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import './modules/navigation.js';
-import './modules/awesomeBooks.js';
+import { listView, addNewView, contactView } from './modules/navigation.js';
+import bookData from './modules/bookObject.js';
+import Awesomebooks from './modules/awesomeBooks.js';
+import { createBooks, sendToLocal } from './modules/createBook.js';
 import showContactInfo from './modules/contact.js';
 import { DateTime } from './modules/luxon.js';
 
@@ -12,3 +14,34 @@ const currentDate = () => {
 window.onload = currentDate();
 
 showContactInfo();
+
+// Navigation links
+listView();
+addNewView();
+contactView();
+
+document.addEventListener('DOMContentLoaded', () => {
+  createBooks();
+});
+
+document.getElementById('add-book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const bookObject = new Awesomebooks(bookData.length, document.getElementById('book-title').value, document.getElementById('book-author').value);
+  Awesomebooks.addBook(bookObject);
+  document.getElementById('book-title').value = '';
+  document.getElementById('book-author').value = '';
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-book')) {
+    let bookData = JSON.parse(localStorage.getItem('storageBooksData'))
+      ? JSON.parse(localStorage.getItem('storageBooksData'))
+      : [];
+
+    const filter = bookData.filter((item) => item.id !== parseInt(event.target.id, 10));
+    bookData = filter;
+    sendToLocal('storageBooksData', bookData);
+    createBooks();
+    window.location.reload();
+  }
+});
